9a490bc44043141ece150423469be8de1f60c51a
Max Kuh <maxes2805582@gmail.com>
2023-11-12 16:15:22 +0200

    Create connect  to DB

--- server/constants.js
+++ server/constants.js
@@ -4,7 +4,10 @@ const CONSTANTS = {
   PORT: process.env.PORT || 6000,
   DB_URL: process.env.DB_URL ||
     `mongodb://localhost:27017`,
-  DB_NAME: process.env.DB_NAME || 'projectDB '
+  DB_HOST: process.env.DB_HOST || 'mongotest.aweennl.mongodb.net',
+  USER_LOGIN: process.env.USER_LOGIN || 'admin',
+  USER_PASSWORD: process.env.USER_PASSWORD || 'admin',
+  DB_NAME: process.env.DB_NAME || 'testDB'
 }
 
 module.exports = CONSTANTS;
---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { USER_LOGIN,USER_PASSWORD,DB_HOST, DB_NAME } = require('../constants');
+
+
+async function connectToDB() {
+  await mongoose.connect(`mongodb+srv://${USER_LOGIN}:${USER_PASSWORD}@${DB_HOST}/${DB_NAME}`) 
+}
+
+connectToDB().catch((err) => console.log(err));
