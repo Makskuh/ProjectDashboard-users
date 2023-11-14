38b23667d898bd5ad4fe023746f3580f1a7ab8be
Max Kuh <maxes2805582@gmail.com>
2023-11-14 16:41:56 +0200

    Add Project model

---
+++ server/models/Project.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const projectSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    text: {type: String, required: true},
+    author: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+  },
+  { timestamps: true }
+);
+
+const Project = model('Project', messageSchema);
+
+module.exports = Project;
