4e2c1ce3827d9b401067a546885fd12bb720dc36
Max Kuh <maxes2805582@gmail.com>
2023-11-14 17:14:27 +0200

    Add Project Router in UserRouter

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const userRouter = require('./userRouter');
 const authRouter = require('./authRouter');
-const projectRouter = require('./projectRouter');
 
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
-router.use('/project',projectRouter)
+
 
 
 module.exports = router;
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,12 +1,14 @@
 const userRouter = require('express').Router();
 const UserController = require('../controller/user.controller');
-
-
+const {checkUser} = require('../middleware/user.mv');
+const {checkAccessToken} = require('../middleware/token.mv')
+const projectRouter = require('./projectRouter')
 userRouter.post('/', UserController.createUser);
 
 userRouter.get('/:userId', UserController.getUser);
 userRouter.put('/:userId', UserController.updateUser);
 userRouter.delete('/:userId', UserController.deleteUser);
 
+userRouter.use('/:userId/projects',checkUser,checkAccessToken, projectRouter)
 
 module.exports = userRouter;
