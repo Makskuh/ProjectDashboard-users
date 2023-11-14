9995f9ceac8ba76a6225effb123d36f530ad0417
Max Kuh <maxes2805582@gmail.com>
2023-11-14 16:58:32 +0200

    Add error handler

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const userRouter = require('./userRouter');
 const authRouter = require('./authRouter');
+const { basicErrorHandler } = require('./middleware/errors');
+const { tokenErrorHandler } = require('./middleware/errors/token');
 
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
+
+router.use(basicErrorHandler);
+router.use(tokenErrorHandler);
 
 module.exports = router;
